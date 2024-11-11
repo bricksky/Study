b112e720c04e1ca42236581680ab9bae5d0b292a
bricksky <skydh1214@naver.com>
2024-11-11 23:41:44 +0900

    p.56 컨트롤러 만들어서 URL 매핑하기

--- Do-it/src/main/java/com/example/doit/DoItApplication.java
+++ Do-it/src/main/java/com/example/doit/DoItApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 프로그램의 시작을 담당하는 파일
 public class DoItApplication {
 
     public static void main(String[] args) {
