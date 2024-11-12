1bc331c4587d1472a0cb1c504fcb7fce4faaba18
bricksky <skydh1214@naver.com>
2024-11-12 19:08:31 +0900

    p.60 URL 매핑

--- Do-it/src/main/java/com/example/doit/MainController.java
+++ Do-it/src/main/java/com/example/doit/MainController.java
@@ -2,11 +2,13 @@ package com.example.doit;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/sbb")
-    public void index() {
-        System.out.println("index");
+    @ResponseBody
+    public String sbb() {
+        return "안녕하세요ㅌㅌ sbb에 오신것을 환영합니다.";
     }
 }
