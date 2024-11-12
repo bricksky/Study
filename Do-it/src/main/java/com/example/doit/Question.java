6e0613a52f6b90638ec82b7f876115fdf8d7e3d5
bricksky <skydh1214@naver.com>
2024-11-12 20:24:46 +0900

    p.71 질문/답변 엔티티 만들기

---
+++ Do-it/src/main/java/com/example/doit/Question.java
@@ -0,0 +1,29 @@
+package com.example.doit;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
