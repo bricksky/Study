183d489bf5631ff25be11256cf63ed8bc0127e67
bricksky <skydh1214@naver.com>
2024-11-12 19:26:36 +0900

    p.64 H2 데이터베이스 설치

--- Do-it/src/main/java/com/example/doit/MainController.java
+++ Do-it/src/main/java/com/example/doit/MainController.java
@@ -9,6 +9,6 @@ public class MainController {
     @GetMapping("/sbb")
     @ResponseBody
     public String sbb() {
-        return "안녕하세요ㅌㅌ sbb에 오신것을 환영합니다.";
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
     }
 }
