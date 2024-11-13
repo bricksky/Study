25ddda249062573c21343a3cb41956a42c375f51
bricksky <skydh1214@naver.com>
2024-11-13 22:31:35 +0900

    p.84 junit 활용

--- Do-it/src/test/java/com/mysite/sbb/DoItApplicationTests.java
+++ Do-it/src/test/java/com/mysite/sbb/DoItApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class DoItApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @Test
+    void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q1.setSubject("스프링 부트 모델 질문입니다.");
+        q1.setContent("id는 자동으로 생성되나요?");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q2);
+    }
 }
