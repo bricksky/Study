21c612b5ed1d843708003238405a7ecfaa92d9f7
bricksky <skydh1214@naver.com>
2024-11-15 22:55:49 +0900

    p.89 findBySubject 매서드

--- Do-it/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ Do-it/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -18,7 +18,7 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-      Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-      assertEquals(1, q.getId());
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
     }
 }
