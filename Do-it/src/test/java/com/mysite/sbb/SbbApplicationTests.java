c4eb460a7b72df658916e098a3f512ddf2f00694
bricksky <skydh1214@naver.com>
2024-11-16 23:11:43 +0900

    p.118 컨트롤러에서 서비스 사용하기

--- Do-it/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ Do-it/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,6 +3,7 @@ package com.mysite.sbb;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,13 +23,24 @@ class SbbApplicationTests {
   @Transactional
     @Test
     void testJpa() {
-        Optional<Question> question = questionRepository.findById(2);
-        assertTrue(question.isPresent());
-        Question q = question.get();
+//        Optional<Question> question = questionRepository.findById(2);
+//        assertTrue(question.isPresent());
+//        Question q = question.get();
+//
+//        List<Answer> answerList = q.getAnswerList();
+//
+//        assertEquals(1, answerList.size());
+//        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+      Question q1 = new Question();
+      q1.setSubject("sbb가 무엇인가요?");
+      q1.setContent("sbb에 대해서 알고싶습니다.");
+      q1.setCreateDate(LocalDateTime.now());
+      this.questionRepository.save(q1);
 
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+      Question q2 = new Question();
+      q2.setSubject("스프링 부트 모델 질문입니다.");
+      q2.setContent("id는 자동으로 생성되나요?");
+      q2.setCreateDate(LocalDateTime.now());
+      this.questionRepository.save(q2);
     }
 }
